59cb0c892f4afe28031bdd6ce2103370ffc9d5a5
maitreyaawad10 <maitreya.awad@gmail.com>
2026-07-13 20:41:03 +0530

    Refactored code - Used slf4j from lombok for logging

--- src/main/java/com/kafka/tutorial/inventory/consumers/InventoryConsumer.java
+++ src/main/java/com/kafka/tutorial/inventory/consumers/InventoryConsumer.java
@@ -1,15 +1,13 @@
 package com.kafka.tutorial.inventory.consumers;
 
 import com.kafka.tutorial.inventory.events.OrderCreatedEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class InventoryConsumer {
-
-    private static final Logger log = LoggerFactory.getLogger(InventoryConsumer.class);
 
     @KafkaListener(topics = "orders")
     public void consume(OrderCreatedEvent orderCreatedEvent) {
