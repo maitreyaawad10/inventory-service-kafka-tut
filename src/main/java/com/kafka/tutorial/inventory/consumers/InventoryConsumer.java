1057f390d774a3e40f535e6790d1cb2334136f4b
maitreyaawad10 <maitreya.awad@gmail.com>
2026-07-18 22:51:17 +0530

    Added manual commit logic

--- src/main/java/com/kafka/tutorial/inventory/consumers/InventoryConsumer.java
+++ src/main/java/com/kafka/tutorial/inventory/consumers/InventoryConsumer.java
@@ -3,6 +3,7 @@ package com.kafka.tutorial.inventory.consumers;
 import com.kafka.tutorial.inventory.events.OrderCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -10,11 +11,12 @@ import org.springframework.stereotype.Service;
 public class InventoryConsumer {
 
     @KafkaListener(topics = "orders")
-    public void consume(OrderCreatedEvent orderCreatedEvent) {
+    public void consume(OrderCreatedEvent orderCreatedEvent, Acknowledgment acknowledgment) {
         if (orderCreatedEvent.customerId() == null)
             throw new IllegalArgumentException("Customer Id is null");
         if (orderCreatedEvent.totalAmount() == null)
             throw new IllegalStateException("Total amount is null");
         log.info("Received order: {}", orderCreatedEvent.orderId());
+        acknowledgment.acknowledge();
     }
 }
